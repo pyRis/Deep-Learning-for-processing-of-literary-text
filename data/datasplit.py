23703079efac1cc0cd8c6eab5516c09c49470b82
Jingyan Chen <74013123+liza0722@users.noreply.github.com>
2023-01-20 22:10:33 +0100

    Update datasplit.py

--- data/datasplit.py
+++ data/datasplit.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import random
+import shutil
+import os
 
 data = pd.read_csv("./Deep-Learning-for-processing-of-literary-text/ge-en-mapping.csv",sep=";")
 de_title = data["german-title"].to_list()
 random.Random(123).shuffle(de_title)
 
+#get lists of german data split 
 train_de = de_title[:27]
 valid_de = de_title[27:31]
 test_de = de_title[31:]
 
+#get lists of english data split 
 train_set = [data.loc[data["german-title"] == title] for title in train_de]
 train_set = pd.concat(train_set,axis=0)
 train_en = train_set["english-title"].to_list()
@@ -22,3 +26,18 @@ test_set = pd.concat(test_set,axis=0)
 test_en = test_set["english-title"].to_list()
 
 print(train_en,valid_en,test_en)
+print(train_de,valid_de,test_de)
+
+#remove “” in play name
+train_en = [play.lstrip('“').rstrip('”') for play in train_en]
+valid_en = [play.lstrip('“').rstrip('”') for play in valid_en]
+test_en = [play.lstrip('“').rstrip('”') for play in test_en]
+
+
+#get english split sets
+for play in train_en:
+    shutil.copyfile(os.path.join("./data/EN",play),os.path.join("./data/EN/train",play))
+for play in valid_en:
+    shutil.copyfile(os.path.join("./data/EN",play),os.path.join("./data/EN/valid",play))
+for play in test_en:
+    shutil.copyfile(os.path.join("./data/EN",play),os.path.join("./data/EN/test",play))
