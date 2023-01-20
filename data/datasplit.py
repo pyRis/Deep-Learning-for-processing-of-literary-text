83b84b8bd5fd01894d8d269b8ec39b4abd327c6e
Jingyan Chen <74013123+liza0722@users.noreply.github.com>
2023-01-20 21:33:45 +0100

    Update datasplit.py

--- data/datasplit.py
+++ data/datasplit.py
@@ -9,11 +9,16 @@ train_de = de_title[:27]
 valid_de = de_title[27:31]
 test_de = de_title[31:]
 
-train_en = [data["english-title"].loc[data["german-title"] == title] for title in train_de]
-# train_set = pd.concat(train_set,axis=0)
+train_set = [data.loc[data["german-title"] == title] for title in train_de]
+train_set = pd.concat(train_set,axis=0)
+train_en = train_set["english-title"].to_list()
 
-valid_en = [data["english-title"].loc[data["german-title"] == title] for title in valid_de]
-# valid_set = pd.concat(valid_set,axis=0)
+valid_set = [data.loc[data["german-title"] == title] for title in valid_de]
+valid_set = pd.concat(valid_set,axis=0)
+valid_en = valid_set["english-title"].to_list()
 
-test_en = [data["english-title"].loc[data["german-title"] == title] for title in test_de]
-# test_set = pd.concat(test_set,axis=0)
+test_set = [data.loc[data["german-title"] == title] for title in test_de]
+test_set = pd.concat(test_set,axis=0)
+test_en = test_set["english-title"].to_list()
+
+print(train_en,valid_en,test_en)
