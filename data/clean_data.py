c086b0f5259dcaa97415ee34f3f5246e81005266
Rishu Kumar <rishu_kumar@live.com>
2023-03-15 19:25:34 +0100

    cleaned text for better chunking

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,5 +1,14 @@
 import re
 from glob import glob
+from sacremoses import MosesTokenizer
+
+
+def standardize(sentence_list: list, lang: str) -> list:
+    mt = MosesTokenizer(lang=lang)
+    for idx, item in enumerate(sentence_list):
+        sentence_list[idx] = mt.tokenize(item, return_str=True, escape=False)
+        print(sentence_list[idx])
+    return sentence_list
 
 
 def preprocessing(file_name):
@@ -28,6 +37,8 @@ def preprocessing(file_name):
                 buffer = line.strip() + ": "
             elif buffer:
                 buffer = buffer + re.sub(r"\[.*?\]", "", line.strip()) + " "
+    lang = file_name.split("/")[0].lower()
+    lines = standardize(lines, lang)
     return lines
 
 
@@ -35,7 +46,7 @@ def main():
     """
     Driver function to run the preprocessing function
     """
-    files = glob("**/full_text.txt", recursive=True)
+    files = glob("EN/**/full_text.txt", recursive=True)
     for item in files:
         new_f_name = item.replace("full_text", "clean_text")
         result = preprocessing(item)
@@ -46,5 +57,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# ToDo: Add more cleaning to normalize text and remove things such as --, ;
