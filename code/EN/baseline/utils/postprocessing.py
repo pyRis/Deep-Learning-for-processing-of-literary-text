7842cea5efe649bcc28287f9a1fe7aec7e3033a7
Rishu Kumar <rishu_kumar@live.com>
2023-03-21 18:32:51 +0100

    removing redundant files

--- code/EN/baseline/utils/postprocessing.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def post_processing(summ:str, person_dict:dict) -> str:
-
-    num_of_person = len(person_dict)
-    for item in reversed(range(num_of_person)):
-        key=f"PERSON{item}"
-        summ = summ.replace(key, person_dict[key]).replace(f"Person{item}", person_dict[key])
-    return summ
