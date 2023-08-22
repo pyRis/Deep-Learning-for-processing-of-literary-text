2b4d3c0750773b99b8c87206d34eabd1faf09d39
Katja <katjakonermann@gmail.com>
2023-08-22 14:37:46 +0200

    Fix paths

--- code/DE/run_test.sh
+++ code/DE/run_test.sh
@@ -1,7 +1,10 @@
 
 search_dir=data/DE/test/*
+out_dir="summary/DE/finetuned/"
 for dir in $search_dir; do
     echo "Inference on play $dir"
     out_file="$(basename $dir)"
-    python3 code/DE/inference.py --input_path $dir --model models/ --output_path $out_file
+    out_path="$out_dir/$out_file"
+    mkdir -p $out_path
+    python3 code/DE/inference.py --input_path $dir --model models/ --output_path $out_path
 done
