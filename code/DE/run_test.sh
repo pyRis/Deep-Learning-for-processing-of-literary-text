8242bbd0bccc4b54b4d08b0ce6f8960caeca8175
Katja Konermann <katjako@tony-4.coli.uni-saarland.de>
2023-08-23 17:29:54 +0200

    Minor edits for bash scripts

--- code/DE/run_test.sh
+++ code/DE/run_test.sh
@@ -6,5 +6,5 @@ for dir in $search_dir; do
     out_file="$(basename $dir)"
     out_path="$out_dir/$out_file"
     mkdir -p $out_path
-    python3 code/DE/inference.py --input_path $dir --model models/ --output_path $out_path
+    python3 code/DE/inference.py --input_path $dir --model models_new/ --output_path $out_path
 done
